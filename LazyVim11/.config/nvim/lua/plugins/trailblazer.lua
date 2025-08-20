1f06eac6c82764a4bed9c4075a9f110ee3d5f82e
ShajTester <kolobrod2002@yandex.ru>
2025-08-20 18:06:36 +0300

    Add Trailblazer to nvim11

---
+++ LazyVim11/.config/nvim/lua/plugins/trailblazer.lua
@@ -0,0 +1,12 @@
+--
+
+return {
+    {
+        "LeonHeidelbach/trailblazer.nvim",
+        config = function()
+            require("trailblazer").setup({
+                -- your custom config goes here
+            })
+        end,
+    },
+}
